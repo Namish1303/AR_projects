1ee20e49a737e404e4950b16980abf8536e4166a
Namish Arora <namish.arora@gmail.com>
2023-10-04 09:46:54 -0400

    First Unity Project
    Trial running of unity project

---
+++ Try/Assets/Scripts/RotateSphere.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSphere : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    Vector3 movement;
+    public int xi, yi, zi;
+    void Start()
+    {
+        movement = new Vector3(xi,yi,zi);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(movement * Time.deltaTime);
+    }
+}
